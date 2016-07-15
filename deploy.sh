0c1bcfeb5874deae38ab437546d2e09e82b349d5
Philipp Holzschneider <philipp@covicon.de>
2016-07-15 02:40:42 +0200

    Made it deployable

---
+++ deploy.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#rm -rf $HOME/deploy
+git clone -b deploy https://${GH_TOKEN}@github.com/${GH_ACCOUNT}/${GH_REPOSITORY} $HOME/deploy
+
+mvn package
+mvn install:install-file -DpomFile=pom.xml -Dfile=$(ls target/*.jar) -DlocalRepositoryPath=$HOME/deploy
+
+cd $HOME/deploy
+
+git config --global user.email "travis@travis-ci.org"
+git config --global user.name "travis-ci"
+
+
+git add .
+git commit -m "travis"
+git push -f origin deploy
