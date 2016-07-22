3850d4db2cb415c184729f8784651e9743256871
Philipp Holzschneider <philipp@covicon.de>
2016-07-22 19:22:59 +0200

    added commands to convert repository to global maven repository style

--- deploy.sh
+++ deploy.sh
@@ -3,14 +3,21 @@
 #rm -rf $HOME/deploy
 git clone -b deploy https://${GH_TOKEN}@github.com/${GH_ACCOUNT}/${GH_REPOSITORY} $HOME/deploy
 
+#'convert' the repository to look like a local repository
+find $HOME/deploy -name "maven-metadata.xml" -exec mv {} $(dirname {})/maven-metadata-local.xml \;
+
+#use maven to package and install the artifact in the repository 
 mvn package
 mvn install:install-file -DpomFile=pom.xml -Dfile=$(ls target/*.jar) -DlocalRepositoryPath=$HOME/deploy
 
+#'convert' the repository back to be a maven repository
+find $HOME/deploy -name "maven-metadata-local.xml" -exec mv {} $(dirname {})/maven-metadata.xml \;
+
+#commit and push the changes to the deploy branch
 cd $HOME/deploy
 
 git config --global user.email "travis@travis-ci.org"
 git config --global user.name "travis-ci"
-
 
 git add .
 git commit -m "travis"
